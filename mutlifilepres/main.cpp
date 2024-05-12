ca5e2d55c18f2f12cc31968578a487f7a710de25
Kamil <your.email@example.com>
2024-05-12 16:25:40 +0200

    przykład jak połączyć deklaracje metody i jej definicje

--- mutlifilepres/main.cpp
+++ mutlifilepres/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include "second.h"
 
+class Foo{ //deklaracja klasy Foo
+public:
+    Foo()
+    {
+        std::cout<< "invocation of Foo ctr\n" ;
+    }
+    void Bar() // deklaracja metody Bar
+    {
+        std::cout << "Hello from Bar metod \n"; // definicja metody Bar
+    }
+};
+
+void Bar(); //deklaracja funkcji
+
 int main () 
 {
     std::cout << " Hello from main" <<  std::endl;
-    foo();
+    Bar();
+    // typ = int ; nazwa = x ; przypisanie wartości 7
+    int              x               =7;
+    Foo y; 
+    y.Bar();    //wywołanie metody
     return 0; 
+}
+
+void Bar()  // definicja funkcji
+{
+    std::cout << "Hello from Bar function \n";
 }
